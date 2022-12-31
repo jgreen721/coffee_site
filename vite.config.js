26928bd5e73b241a3b8928d493159a6808eb6c18
Justin Green <justingreen@Justins-MacBook-Air.local>
2022-12-31 16:30:49 -0500

    save

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://jgreen721.github.io/coffee_site",
+  base: "https://jgreen721.github.io/",
 });
